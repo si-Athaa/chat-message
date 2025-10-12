dd7fee340e76691c13e5f14a8609d6919b939947
Athaa <athanaya.sakhy1@fiwa.sch.id>
2025-10-12 17:17:18 +0700

    Create wow.cpp

---
+++ wow.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Atha";
+    cout << "Hai " << nama << endl;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Atha";
+    cout << "Hai " << nama << endl;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Atha";
+    cout << "Hai " << nama << endl;
+    return 0;
+}
